461dfbf0dd1294fb5acd5a005a3df697ecc0b566
조민수 <87609692+talli0505@users.noreply.github.com>
2022-09-26 08:04:55 +0900

    16. 문자열 내 p와 y의 개수
    출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges

---
+++ practice/16_문자열_내_p와_y의_개수.js
@@ -0,0 +1,30 @@
+대문자와 소문자가 섞여있는 문자열 s가 주어집니다.
+s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
+'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
+문자열 s의 길이 : 50 이하의 자연수
+문자열 s는 알파벳으로만 이루어져 있습니다.
+   s	        answer
+"pPoooyY"	  true
+ "Pyy"	      false
+'p'의 개수 2개, 'y'의 개수 2개로 같으므로 true를 return 합니다.
+
+
+function solution(s){                 
+  var answer = true;                  // 값을 받을 변수
+  let change = s.toLowerCase();       // 대문자 소문자 다있으니깐 전체를 소문자로 변경
+
+  let p = change.split('p').length;   // p자리에서 나누고 그 길이를 대입
+  let y = change.split('y').length;   // y자리에서 나누고 그 길이를 대입
+
+  if(p == y) {                        // p, y 길이가 같으면
+    answer = true                     // 정답에 true
+  }
+  else {                              // p, y 길이가 다르면
+    answer = false                    // 정답에 false
+  }
+  return answer;                      // 정답을 보여줌
+}
+
+
+출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
