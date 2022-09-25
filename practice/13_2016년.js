0309feb60c8c3d762f0d6f4241328cf25a2ba778
조민수 <87609692+talli0505@users.noreply.github.com>
2022-09-25 10:58:41 +0900

    13. 2016년
    출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges

---
+++ practice/13_2016년.js
@@ -0,0 +1,18 @@
+2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요?
+두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요.
+요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
+입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 "TUE"를 반환하세요.
+2016년은 윤년입니다.
+2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+a	b	result
+5	24	"TUE"
+
+
+function solution(a, b) {
+  let day = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];    // 요일 다 선언
+  let date = new Date(`2016-${a}-${b}`);                          // 백틱으로 a,b를 가져와서 그 날짜를 선언
+  return day[date.getDay()];                                      // 선언한 날짜의 요일 가져와서 대입후 가져오기
+}
+
+
+출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
