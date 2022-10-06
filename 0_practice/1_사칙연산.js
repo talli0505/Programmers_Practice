6f65a9d0d9c441c95c14e160e9b491114b3da943
조민수 <87609692+talli0505@users.noreply.github.com>
2022-10-06 16:41:35 +0900

    1. 사칙연산
    출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges

---
+++ 0_practice/1_사칙연산.js
@@ -0,0 +1,29 @@
+1. 두 수의 합
+
+function solution(num1, num2) {
+  return num1 + num2
+}
+
+
+2. 두 수의 차
+
+function solution(num1, num2) {
+  return num1 - num2
+}
+
+
+3. 두 수의 곱
+
+function solution(num1, num2) {
+  return num1 * num2
+}
+
+
+4. 몫 구하기
+
+function solution(num1, num2) {
+  return Math.floor(num1 / num2)
+}
+
+
+출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
