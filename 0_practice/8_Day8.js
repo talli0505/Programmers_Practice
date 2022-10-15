e0f4067f5b565babf7e67bb941663b78cdae5fc0
조민수 <87609692+talli0505@users.noreply.github.com>
2022-10-15 13:26:03 +0900

    Day8
    출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges

---
+++ 0_practice/8_Day8.js
@@ -0,0 +1,40 @@
+1. 배열 자르기
+
+
+function solution(numbers, num1, num2) {
+  let answer = []
+  for(let i = num1 ; i <= num2 ; i++) {
+    answer.push(numbers[i])
+  }
+  return answer
+}
+
+
+2. 외계행성의 나이
+
+
+function solution(age) {
+  let answer = '';
+  let b = age.toString().split("")
+  for(let i = 0; i < b.length ; i++) {
+    answer += String.fromCharCode(Number(b[i]) + 97)
+  } 
+  return answer
+}
+
+
+3. 순서쌍의 개수
+
+
+function solution(n) {
+  let answer = 0
+  for(let i = 1; i <= n; i++) {
+    if(n % i === 0) {
+      answer++
+    }
+  }
+  return answer
+}
+
+
+출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
