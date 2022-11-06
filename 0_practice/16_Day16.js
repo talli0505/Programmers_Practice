b025a06628c0634220cfb44e77d0b6120d0229b2
조민수 <87609692+talli0505@users.noreply.github.com>
2022-11-06 15:17:05 +0900

    Day16
    출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges

---
+++ 0_practice/16_Day16.js
@@ -0,0 +1,37 @@
+1. 편지
+
+function solution(message) {
+  return message.length *2
+}
+
+
+2. 가장 큰 수 찾기
+
+function solution(array) {
+  let a = Math.max(...array)
+  let b = 0
+  for(let i = 0; i < array.length; i++) {
+    if(a === array[i]) {
+      b = i
+    }
+  }
+  return [a,b]
+}
+
+
+3. 배열의 유사도
+
+function solution(s1, s2) {
+  let count = 0
+  s1 = s1.sort()
+  s2 = s2.sort()
+  for(let i = 0; i < s2.length; i++) {
+    if(s2.includes(s1[i])) {
+      count ++
+    }
+  }
+  return count
+}
+
+
+출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
